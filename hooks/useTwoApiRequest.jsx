dc2376cb250ffdba3d511de7cc57bcb4bdb70f9a
sedmima20 <sedmima20@sps-prosek.cz>
2024-04-09 11:16:14 +0200

    Vytvořen custom React hook zajišťující komunikaci s API

---
+++ hooks/useTwoApiRequest.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+
+const useTwoApiRequest = () => {
+    const [isLoading, setIsLoading] = useState(false);
+    const [responseData, setResponseData] = useState(undefined);
+    const [statusCode, setStatusCode] = useState(undefined);
+    const [isRequestError, setIsRequestError] = useState(false);
+
+    const fetchData = async (requestData) => {
+        setIsLoading(true);
+        try {
+            const response = await fetch('https://sedmima20.sps-prosek.cz/projekty/php/turnajovy-software/index.php', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                },
+                body: new URLSearchParams(requestData),
+            });
+
+            setStatusCode(response.status);
+            setResponseData(await response.json());
+            setIsRequestError(false);
+        } catch {
+            setStatusCode(undefined);
+            setResponseData(undefined);
+            setIsRequestError(true);
+        } finally {
+            setIsLoading(false);
+        }
+    };
+};
+
+export default useTwoApiRequest;
