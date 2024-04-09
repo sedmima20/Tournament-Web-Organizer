cd73f39c4ba4445123734d0e0654ef6794e554b6
Martin Sedmihradský <100826013+sedmima20@users.noreply.github.com>
2024-04-09 13:31:36 +0200

    Přidána podpora různých datových typů pro localStorage hook

--- hooks/useLocalStorageState.jsx
+++ hooks/useLocalStorageState.jsx
@@ -3,11 +3,15 @@ import { useState, useEffect } from 'react';
 export default function useLocalStorageState(key, defaultValue) {
     const [state, setState] = useState(() => {
         const storedValue = localStorage.getItem(key);
-        return storedValue ? storedValue : defaultValue;
+        try {
+            return storedValue !== null ? JSON.parse(storedValue) : defaultValue;
+        } catch {
+            return storedValue;
+        }
     });
 
     useEffect(() => {
-        localStorage.setItem(key, state);
+        localStorage.setItem(key, JSON.stringify(state));
     }, [state]);
 
     return [state, setState];
