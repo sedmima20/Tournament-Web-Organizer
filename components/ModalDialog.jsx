5acb31d98362eab6384931df6da17d87c79af58f
sedmima20 <sedmima20@sps-prosek.cz>
2024-04-20 16:38:10 +0200

    DialogBox komponenta přejmenována na ModalDialog

--- components/ModalDialog.jsx
+++ components/ModalDialog.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef } from 'react'
 
-export default function DialogBox(props) {
+export default function ModalDialog(props) {
     const dialogRef = useRef(null)
 
     // Automatické otevření a zavření vanilla HTML/JS dialogového okna při vyrenderování a při destrukci komponenty
