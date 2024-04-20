f26adbeff97192fa3ec8b2a79e4832cd3d527b5d
sedmima20 <sedmima20@sps-prosek.cz>
2024-04-20 16:23:57 +0200

    Vytvořena logika pro real-time kontrolování připojení k internetu a podmínečně renderovaný warning box - Kontrola připojení k internetu (respektive k auth/resource serveru) probíhá při prvním vyrenderování hlavní komponenty a poté každých 10 sekund - Funkce využívá stahování velmi malého obrázku (1x1 pixel, 34 bytů) ze serveru pro ověření, zda spojení se serverem stále funguje. - Další nápady na potenciální zmenšení zátěže ke zvážení:      - .pbm obrázky s menší velikostí 8 bytů, ale vypadá to, že možná nejsou kompatibilní s img.src      - http místo https, ale nemusí to být bezpečné      - head requesty místo get, pokud by odpovědi byly menší

--- App.jsx
+++ App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { HashRouter as Router, Route, Routes, Link } from 'react-router-dom'
 import useLocalStorageState from '/hooks/useLocalStorageState.jsx'
 import useTwoApiRequest from '/hooks/useTwoApiRequest.jsx'
@@ -10,7 +10,9 @@ import logo from '/images/logo.png'
 
 export default function App() {
     const [token, setToken] = useLocalStorageState('authToken', '')
-    const intervalRef = useRef(null)
+    const [isConnected, setIsConnected] = useState(true)
+    const checkTokenIntervalRef = useRef(null)
+    const checkConnectionIntervalRef = useRef(null)
     const checkTokenRequest = useTwoApiRequest({
         endpoint: 'is_token_valid',
         token: token
@@ -20,12 +22,23 @@ export default function App() {
     useEffect(() => {
         checkToken()
 
-        intervalRef.current = setInterval(checkToken, 30000)
+        checkTokenIntervalRef.current = setInterval(checkToken, 30000)
 
         return () => {
-            clearInterval(intervalRef.current)
+            clearInterval(checkTokenIntervalRef.current)
         }
     }, [token])
+
+    // Kontrola připojení k internetu (respektive k auth/resource serveru) při prvním vyrenderování hlavní komponenty a poté každých 10 sekund
+    useEffect(() => {
+        checkConnection()
+
+        checkConnectionIntervalRef.current = setInterval(checkConnection, 10000)
+
+        return () => {
+            clearInterval(checkConnectionIntervalRef.current)
+        }
+    }, [])
 
     // Nastavení tokenu na prázdný string, pokud je neplatný, plus informování uživatele. Zbytek aplikace už počítá s tím, že prázdný string je neplatný token a kterýkoli jiný string je platný token.
     const checkToken = () => {
@@ -37,6 +50,25 @@ export default function App() {
                         alert("Jejda, přihlášení vypršelo :( Pokud bys i nadále rád(a) spravoval(a) své turnaje, znovu se prosím přihlas.")
                     }
                 })
+        }
+    }
+
+    // Funkce využívá stahování velmi malého obrázku (1x1 pixel, 34 bytů) ze serveru pro ověření, zda spojení se serverem stále funguje.
+    // Další nápady na potenciální zmenšení zátěže ke zvážení:
+    //    - .pbm obrázky s menší velikostí 8 bytů, ale vypadá to, že možná nejsou kompatibilní s img.src
+    //    - http místo https, ale nemusí to být bezpečné
+    //    - head requesty místo get, pokud by odpovědi byly menší
+    const checkConnection = () => {
+        const img = new Image()
+
+        img.src = 'https://sedmima20.sps-prosek.cz/projekty/php/turnajovy-software/c.webp?' + new Date().getTime()
+
+        img.onload = () => {
+            setIsConnected(true)
+        }
+
+        img.onerror = () => {
+            setIsConnected(false)
         }
     }
 
@@ -58,6 +90,7 @@ export default function App() {
                 </ul>
             </nav>
             <main>
+                {!isConnected && <div className="warning-box">Buď náš server podal výpověď, nebo tvé připojení k internetu udělalo pápá 👋</div>}
                 <Routes>
                     <Route path="/" element={<HomePage />} />
                     <Route path="/login" element={<LoginPage />} />
