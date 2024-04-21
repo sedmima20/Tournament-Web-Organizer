59787bfa7d0af77fd0d3b04c1d7c827ea976c79f
sedmima20 <sedmima20@sps-prosek.cz>
2024-04-21 14:47:59 +0200

    Vytvořen soubor pro stránku turnajů (zatím jen stručný placeholder bez logiky)

---
+++ pages/TournamentsPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function TournamentsPage() {
+    return (
+        <>
+            <section>
+                <h1>Moje turnaje</h1>
+                <p>Lorem ipsum</p>
+                <p>Lorem ipsum</p>
+            </section>
+        </>
+    )
+}
