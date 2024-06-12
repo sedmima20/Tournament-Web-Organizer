c03a365fce6f3f2602c01b84758f9781b9272c41
sedmima20 <sedmima20@sps-prosek.cz>
2024-06-12 17:27:34 +0200

    Zjednodušen kód pro zachytávání události zavření modálního dialogového okna

--- components/ModalDialog.jsx
+++ components/ModalDialog.jsx
@@ -3,28 +3,20 @@ import React, { useEffect, useRef } from 'react'
 export default function ModalDialog(props) {
     const dialogRef = useRef(undefined)
 
+    // Zachytávání události zavření dialogového okna pro případ, že k tomu dojde jiným způsobem, než destrukcí komponenty (např. zmáčknutím klávesy Esc). Nadřazená komponenta tuto událost zachytí a destrukci může provést dodatečně.
     function handleModalDialogClose() {
         if (props.onClose) {
             props.onClose()
         }
     }
 
+    // Automatické otevření vanilla HTML/JS modálního dialogového okna při vyrenderování komponenty
     useEffect(() => {
-        // Automatické otevření vanilla HTML/JS modálního dialogového okna při vyrenderování komponenty
         dialogRef.current.showModal()
-
-        // Zachytávání události zavření dialogového okna pro případ, že k tomu dojde jiným způsobem, než destrukcí komponenty (např. zmáčknutím klávesy Esc). Nadřazená komponenta tuto událost zachytí a destrukci může provést dodatečně.
-        dialogRef.current.addEventListener('close', handleModalDialogClose)
-
-        return () => {
-            if (dialogRef.current) {
-                dialogRef.current.removeEventListener('close', handleModalDialogClose)
-            }
-        }
     }, [])
 
     return (
-        <dialog ref={dialogRef}>
+        <dialog ref={dialogRef} onClose={handleModalDialogClose}>
             {props.children}
         </dialog>
     )
